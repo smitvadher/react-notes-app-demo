11520c406a85eefb61d60deabf9a0c080d7d608f
Smit Vadher <vadhersmit@gmail.com>
2024-03-15 19:16:02 -0400

    Moved window size detection functionality to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
 import Notes from "./Components/Note/Notes";
 import NotePopup from "./Components/Note/NotePopup";
 import NoteManagerProvider from "./Context/NoteManagerProvider";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [sizeClass, setSizeClass] = useState("");
+
+  const matchesWidth = (width: number) => {
+    return window.matchMedia(`(max-width: ${width}px)`).matches;
+  };
+
+  const getSize = () => {
+    if (matchesWidth(768)) {
+      return "sm";
+    } else if (matchesWidth(992)) {
+      return "md";
+    } else if (matchesWidth(1200)) {
+      return "lg";
+    }
+    return "";
+  };
+
+  const resizeListener = () => {
+    setSizeClass(getSize());
+  };
+
+  useEffect(() => {
+    resizeListener();
+    window.addEventListener("resize", resizeListener);
+    return () => {
+      window.removeEventListener("resize", resizeListener);
+    };
+  }, []);
+
   return (
     <>
-      <div className="container">
+      <div className={`container ${sizeClass}`}>
         <h1 className="text-center">Notes</h1>
         <NoteManagerProvider>
           <NotePopup></NotePopup>
--- src/Components/Note/Notes.tsx
+++ src/Components/Note/Notes.tsx
@@ -14,35 +14,6 @@ const Notes = () => {
   const pinnedNotes = notes.filter((note) => note.isPinned);
   const otherNotes = notes.filter((note) => !note.isPinned);
 
-  const [gridSize, setGridSize] = useState("");
-
-  const matchesWidth = (width: number) => {
-    return window.matchMedia(`(max-width: ${width}px)`).matches;
-  };
-
-  const getSize = () => {
-    if (matchesWidth(768)) {
-      return "sm";
-    } else if (matchesWidth(992)) {
-      return "md";
-    } else if (matchesWidth(1200)) {
-      return "lg";
-    }
-    return "";
-  };
-
-  const resizeListener = () => {
-    setGridSize(getSize());
-  };
-
-  useEffect(() => {
-    resizeListener();
-    window.addEventListener("resize", resizeListener);
-    return () => {
-      window.removeEventListener("resize", resizeListener);
-    };
-  }, []);
-
   return (
     <>
       <div className="button mb-3" onClick={() => setAddNotePopup(true)}>
@@ -65,7 +36,7 @@ const Notes = () => {
       {notes.length === 0 ? (
         <div>No notes</div>
       ) : (
-        <div className={gridSize}>
+        <div>
           {pinnedNotes.length > 0 && (
             <div className="section">
               <h4>Pinned</h4>
